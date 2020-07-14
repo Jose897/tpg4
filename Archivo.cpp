315878a52ac5688fc4fdf3fa85ef5cbcd29998e7
lebarrionuevo <66890327+lebarrionuevo@users.noreply.github.com>
2020-07-13 21:13:38 -0300

    Actualizado con excepcion

--- Archivo.cpp
+++ Archivo.cpp
@@ -1,10 +1,20 @@
 #include "Archivo.h"
 #include <fstream>
+
+const string EXCEPCION = "Error al encontrar el archivo solicitado";
+
 Archivo::Archivo() {
+    excepcion = false;
 }
 
 Archivo:: ~Archivo() {
+}
 
+bool Archivo::hay_excepcion(){
+    if(excepcion){
+        cout << endl << "Lo lamento, el programa no se logro ejecutar por la inexistencia de archivos solicitados :(" << endl;
+    }
+    return excepcion;
 }
 
 void Archivo::carga_peliculas(const string nombre_archivo, Lista<Pelicula*> &lista){
@@ -14,29 +24,33 @@ void Archivo::carga_peliculas(const string nombre_archivo, Lista<Pelicula*> &lis
     string puntaje_pelicula;
     string actores_pelicula;
     string espacio;
-    ifstream archivo(nombre_archivo);
-    if(archivo.is_open()){
-        while(!archivo.eof()){
-            getline(archivo, titulo_pelicula);
-            getline(archivo, genero_pelicula);
-            getline(archivo, puntaje_pelicula);
-            getline(archivo, director_pelicula);
-            getline(archivo, actores_pelicula);
-            getline(archivo, espacio);
-            stringstream linea_puntaje(puntaje_pelicula);
-            int puntaje;
-            linea_puntaje >> puntaje;
-            Pelicula *pelicula = new Pelicula(titulo_pelicula, genero_pelicula, puntaje, director_pelicula,separar_actores(actores_pelicula));
-            lista.insertar(pelicula);
+    try{
+        ifstream archivo(nombre_archivo);
+        if(archivo.is_open()){
+            while(!archivo.eof()){
+                getline(archivo, titulo_pelicula);
+                getline(archivo, genero_pelicula);
+                getline(archivo, puntaje_pelicula);
+                getline(archivo, director_pelicula);
+                getline(archivo, actores_pelicula);
+                getline(archivo, espacio);
+                stringstream linea_puntaje(puntaje_pelicula);
+                int puntaje;
+                linea_puntaje >> puntaje;
+                Pelicula *pelicula = new Pelicula(titulo_pelicula, genero_pelicula, puntaje, director_pelicula,separar_actores(actores_pelicula));
+                lista.insertar(pelicula);
+            }
+            archivo.close();
         }
-        archivo.close();
+        else{
+            throw EXCEPCION;
+        }
     }
-    else{
-        throw "El archivo no existe!!!\n";
+    catch(const string e){
+        excepcion = true;
+        cout << "Ha ocurrido una excepcion del tipo '" << e << "'." << endl;
     }
 }
-
-
 
 Lista <string*>* Archivo::separar_actores(string actores_pelicula){
 	Lista <string*>* l_actores = new Lista <string*>;
@@ -52,12 +66,12 @@ Lista <string*>* Archivo::separar_actores(string actores_pelicula){
 	return l_actores;
 }
 /*Lista<Pelicula*> Archivo::carga_lista_recomendadas(Lista<Pelicula*> &lista_vistas, Lista<Pelicula*> &lista_no_vistas){
-    
+
 	Lista<Pelicula*> lista_recomendadas;
 	for (unsigned i = 1; i <= lista_no_vistas.obtener_tamanio(); i++) {
     	if(lista_no_vistas.obtener_dato(i)->obtener_puntaje() >= 7 ){
                 lista_recomendadas.insertar(lista_no_vistas.obtener_dato(i));
-	}else{	
+	}else{
 		for(unsigned j = 1; j <= lista_vistas.obtener_tamanio(); j++){
 			if(lista_no_vistas.obtener_dato(i)->obtener_genero() == lista_vistas.obtener_dato(j)->obtener_genero() ){
 				if( lista_no_vistas.obtener_dato(i)->obtener_director() == lista_vistas.obtener_dato(j)->obtener_director() ){
@@ -67,7 +81,7 @@ Lista <string*>* Archivo::separar_actores(string actores_pelicula){
 					Lista<string*>* puntero_no_vistas;
 					puntero_vistas = lista_vistas.obtener_dato(j)->obtener_actores();
 					puntero_no_vistas = lista_no_vistas.obtener_dato(i)->obtener_actores();
-						
+
 					for(unsigned k = 1; k <=puntero_no_vistas->obtener_tamanio() ; k++){
 						for(unsigned l = 1; l <=puntero_vistas->obtener_tamanio() ; l++){
 							if( *puntero_no_vistas->obtener_dato(k) == *puntero_vistas->obtener_dato(l) ){
@@ -75,10 +89,9 @@ Lista <string*>* Archivo::separar_actores(string actores_pelicula){
 							}
 						}
 					}
-
 				}
-			}	
-			
+			}
+
 		}
 		}
     }
@@ -87,20 +100,20 @@ Lista <string*>* Archivo::separar_actores(string actores_pelicula){
 
 Lista<Pelicula*> Archivo::carga_lista_recomendadas(Lista<Pelicula*> &lista_vistas, Lista<Pelicula*> &lista_no_vistas){
     Lista<Pelicula*> lista_recomendadas;
-    for (int i = 1; i <= lista_no_vistas.obtener_tamanio(); i++) {
+    for (int i = 1; i <= int(lista_no_vistas.obtener_tamanio()); i++) {
         if( lista_no_vistas.obtener_dato(i)->obtener_puntaje() >= 7){
             lista_recomendadas.insertar(lista_no_vistas.obtener_dato(i));
         }
         else{
-            for(int j = 1; j<=lista_vistas.obtener_tamanio(); j++) {
+            for(int j = 1; j <= int(lista_vistas.obtener_tamanio()); j++) {
                 if(lista_vistas.obtener_dato(j)->obtener_genero() == lista_no_vistas.obtener_dato(i)->obtener_genero()) {
                     lista_recomendadas.insertar(lista_no_vistas.obtener_dato(i));
                     if(lista_vistas.obtener_dato(j)->obtener_director() == lista_no_vistas.obtener_dato(i)->obtener_director()){
                         lista_recomendadas.insertar(lista_no_vistas.obtener_dato(i));
                     }
                     else{
-                        for (int k = 1; k <= lista_no_vistas.obtener_dato(i)->obtener_actores()->obtener_tamanio(); k++){
-                            for (int z = 1; z <= lista_vistas.obtener_dato(j)->obtener_actores()->obtener_tamanio(); z++){
+                        for (int k = 1; k <= int(lista_no_vistas.obtener_dato(i)->obtener_actores()->obtener_tamanio()); k++){
+                            for (int z = 1; z <= int(lista_vistas.obtener_dato(j)->obtener_actores()->obtener_tamanio()); z++){
                                 if (*lista_no_vistas.obtener_dato(i)->obtener_actores()->obtener_dato(k) == *lista_vistas.obtener_dato(j)->obtener_actores()->obtener_dato(z)){
                                     lista_recomendadas.insertar(lista_no_vistas.obtener_dato(i));
                                 }
